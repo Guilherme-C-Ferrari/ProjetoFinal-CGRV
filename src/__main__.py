c8cbfd67c2c0fe29abf1e16010560b61a4f03849
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-07-09 05:12:43 -0300

    Atualização do Fluxo de PDI

--- src/__main__.py
+++ src/__main__.py
@@ -11,31 +11,62 @@ def show_image(window_name: str, image):
     cv2.destroyAllWindows
 
 def process_image(image):
-    color_segmented_image = segment_image_by_color(image)
-    return color_segmented_image
+    blurred_image = blur_image(image)
+    upper_half, lower_half = split_image(blurred_image)
+    segmented_upper_half, segmented_lower_half = segment_image_by_color(upper_half, lower_half)
+    reconnected_image = reconnect_image(segmented_upper_half, segmented_lower_half)
+    return sharpen_image(reconnected_image)
 
-def segment_image_by_color(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+def split_image(image):
+    h = image.shape[0]
+    half = h // 2
 
-    lower_blue = np.array([90, 50, 50])
-    upper_blue = np.array([130, 255, 255])
-    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    upper_half = image[:half, :]
+    lower_half = image[half:, :] 
 
-    lower_red = np.array([0, 70, 50])
-    upper_red = np.array([25, 255, 255])
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+    return upper_half, lower_half
 
-    lower_red2 = np.array([170, 70, 50])
-    upper_red2 = np.array([180, 255, 255])
-    mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    
-    masked_red = cv2.bitwise_and(image, image, mask=mask_red)
-    masked_red2 = cv2.bitwise_and(image, image, mask=mask_red2)
-    masked_blue = cv2.bitwise_and(image, image, mask=mask_blue)
+def reconnect_image(upper_half, lower_half):
+    reconnected_image = cv2.vconcat([upper_half, lower_half])
+    return reconnected_image
 
-    masked_final = cv2.bitwise_or(masked_red, masked_red2)
+def segment_image_by_color(upper_half, lower_half):
+    hsv_upper = cv2.cvtColor(upper_half, cv2.COLOR_BGR2HSV)
+    hsv_lower = cv2.cvtColor(lower_half, cv2.COLOR_BGR2HSV)
 
-    return masked_final
+    limit_lo_red1 = np.array([0, 5, 50])
+    limit_up_red1 = np.array([15, 255, 255])
+    limit_lo_red2 = np.array([170, 5, 50])
+    limit_up_red2 = np.array([180, 255, 255])
+
+    limit_lo_red3 = np.array([0, 80, 50])
+    limit_up_red3 = np.array([15, 255, 255])
+    limit_lo_red4 = np.array([170, 80, 50])
+    limit_up_red4 = np.array([180, 255, 255])
+
+    upper_half_mask_red = cv2.inRange(hsv_upper, limit_lo_red1, limit_up_red1)
+    upper_half_mask_red2 = cv2.inRange(hsv_upper, limit_lo_red2, limit_up_red2)
+    upper_half_masked_red = cv2.bitwise_and(upper_half, upper_half, mask=upper_half_mask_red)
+    upper_half_masked_red2 = cv2.bitwise_and(upper_half, upper_half, mask=upper_half_mask_red2)
+    upper_half_masked_final = cv2.bitwise_or(upper_half_masked_red, upper_half_masked_red2)
+
+    lower_half_mask_red = cv2.inRange(hsv_lower, limit_lo_red3, limit_up_red3)
+    lower_half_mask_red2 = cv2.inRange(hsv_lower, limit_lo_red4, limit_up_red4)
+    lower_half_masked_red = cv2.bitwise_and(lower_half, lower_half, mask=lower_half_mask_red)
+    lower_half_masked_red2 = cv2.bitwise_and(lower_half, lower_half, mask=lower_half_mask_red2)
+    lower_half_masked_final = cv2.bitwise_or(lower_half_masked_red, lower_half_masked_red2)
+
+    return upper_half_masked_final, lower_half_masked_final
+
+def blur_image(image):
+    return cv2.blur(image, (5,5))
+
+def sharpen_image(image):
+    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    return cv2.filter2D(image, -1, sharpen_kernel)
+
+def change_color(image):
+    pass
 
 def main():
     paths = import_images()
