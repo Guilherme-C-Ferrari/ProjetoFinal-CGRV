39ec9b2094f0339705c822fbb37699403d66dc72
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-07-09 21:13:56 -0300

    Função change_color funcional

--- src/__main__.py
+++ src/__main__.py
@@ -12,10 +12,10 @@ def show_image(window_name: str, image):
 
 def process_image(image):
     blurred_image = blur_image(image)
-    upper_half, lower_half = split_image(blurred_image)
+    upper_half, lower_half = split_image(image)
     segmented_upper_half, segmented_lower_half = segment_image_by_color(upper_half, lower_half)
     reconnected_image = reconnect_image(segmented_upper_half, segmented_lower_half)
-    return sharpen_image(reconnected_image)
+    return change_color(image, reconnected_image)
 
 def split_image(image):
     h = image.shape[0]
@@ -34,9 +34,9 @@ def segment_image_by_color(upper_half, lower_half):
     hsv_upper = cv2.cvtColor(upper_half, cv2.COLOR_BGR2HSV)
     hsv_lower = cv2.cvtColor(lower_half, cv2.COLOR_BGR2HSV)
 
-    limit_lo_red1 = np.array([0, 5, 50])
+    limit_lo_red1 = np.array([0, 10, 50])
     limit_up_red1 = np.array([15, 255, 255])
-    limit_lo_red2 = np.array([170, 5, 50])
+    limit_lo_red2 = np.array([150, 10, 50])
     limit_up_red2 = np.array([180, 255, 255])
 
     limit_lo_red3 = np.array([0, 80, 50])
@@ -61,12 +61,31 @@ def segment_image_by_color(upper_half, lower_half):
 def blur_image(image):
     return cv2.blur(image, (5,5))
 
-def sharpen_image(image):
-    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    return cv2.filter2D(image, -1, sharpen_kernel)
+# def sharpen_image(image):
+#     sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+#     return cv2.filter2D(image, -1, sharpen_kernel)
 
-def change_color(image):
-    pass
+def change_color(original_image, image):
+    show_image("Segmentado", image)
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    show_image("HSV", hsv)
+
+    h,s,v = cv2.split(hsv)
+    h = (h + 70) % 180
+    hsv_modificado = cv2.merge([h,s,v])
+    
+    show_image("HSV_Modificado", hsv_modificado)
+
+    bgr = cv2.cvtColor(hsv_modificado, cv2.COLOR_HSV2BGR)
+
+    show_image("BGR", bgr)
+
+    null_value = np.array([0,0,0])
+    inverted_mask = cv2.inRange(bgr, null_value, null_value)
+    inverted_masked = cv2.bitwise_and(original_image, original_image, mask=inverted_mask)
+    return cv2.bitwise_or(inverted_masked, bgr)
 
 def main():
     paths = import_images()
